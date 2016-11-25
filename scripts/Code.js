eb9fb9cad58ff4191fc2224c5da38bd5b793c79e
Bruce McPherson <bruce@mcpher.com>
2016-11-25 11:23:07 +0000

    updated by GasGit automation

--- scripts/Code.js
+++ scripts/Code.js
@@ -4,7 +4,8 @@
 function doGet() {
   return HtmlService.createTemplateFromFile('index')
     .evaluate()
-    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 
 }
 /**
